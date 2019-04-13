f277220afa1b055307c964e37fced18feeb34ba9
Rishabh2324 <rishabh2401jain@gmail.com>
2019-04-13 13:08:43 +0530

    sdgfSegf

---
+++ Todo Project/js/todo.js
@@ -0,0 +1,49 @@
+var typed = new Typed('h1', {
+  strings: ["Eter you todo ", "Enter yet todo"],
+  typeSpeed:60
+});
+
+
+var add= document.getElementById('add');
+var removeall = document.getElementById ('removeall');
+var list = document.getElementById ('list');
+
+
+removeall.onclick = function(){
+list.innerHTML='';
+}
+
+add.onclick = function(){
+    addlis(list);   
+    document.getElementById('userinput').value='';
+    document.getElementById('userinput').focus();
+}   
+function addlis(targetUl) {
+    var inputText = document.getElementById ("userinput").value;
+    var li = document.createElement('li');
+    var textNode = document.createTextNode(inputText + '');
+    var removeButton = document.createElement('button');
+
+    if (inputText !==''){
+        removeButton.className='btn-xs  btn-danger';
+        removeButton.innerHTML = '&times';
+        removeButton.setAttribute('onclick','removeMe(this)');
+       
+        li.appendChild(textNode);
+        li.appendChild(removeButton);
+        targetUl.appendChild(li);
+        
+    }else {
+        alert("PLease enter a todo");
+    }
+}
+
+ function removeMe(item){
+     var p =item.parentElement;
+     p.parentElement.removeChild(p);
+ }
+
+
+
+
+
